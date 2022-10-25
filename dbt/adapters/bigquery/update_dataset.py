e4abbb3098bb693294936ead24c48de0c2856d29
Colin <colin.rogers@dbtlabs.com>
2022-10-25 15:44:00 -0700

    formatting

--- dbt/adapters/bigquery/update_dataset.py
+++ dbt/adapters/bigquery/update_dataset.py
@@ -1,5 +1,3 @@
-
-
 from threading import Thread
 
 import google.cloud.bigquery
@@ -8,13 +6,15 @@ import queue
 DATASET_UPDATE_QUEUE = queue.SimpleQueue()
 __DATASET_UPDATE_THREAD = {}
 
+
 class DatasetAsyncUpdater(Thread):
     client: google.cloud.bigquery.Client
+
     def __init__(self, client):
         super().__init__()
         self.daemon = True
         self.client = client
-    
+
     def run(self):
         while True:
             val = DATASET_UPDATE_QUEUE.get()
@@ -24,9 +24,9 @@ class DatasetAsyncUpdater(Thread):
 
 def start_dataset_update_thread(client):
     if "thread" in __DATASET_UPDATE_THREAD:
-        dataset_update_thread: DatasetAsyncUpdater = __DATASET_UPDATE_THREAD['thread']
+        dataset_update_thread: DatasetAsyncUpdater = __DATASET_UPDATE_THREAD["thread"]
         if not dataset_update_thread.is_alive():
             dataset_update_thread.start()
     dataset_update_thread = DatasetAsyncUpdater(client=client)
     dataset_update_thread.start()
-    __DATASET_UPDATE_THREAD['thread'] = dataset_update_thread
+    __DATASET_UPDATE_THREAD["thread"] = dataset_update_thread
