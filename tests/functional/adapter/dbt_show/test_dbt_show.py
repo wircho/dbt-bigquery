e24894da07bf79688e468dac0e05d539f213fc79
Matthew McKnight <91097623+McKnight-42@users.noreply.github.com>
2023-09-28 13:35:22 -0500

    Revert "Add dbt show tests (#927)"
    This reverts commit 0fe8afc105ed196bcd201c0c487f03e2f93aba8b.

--- tests/functional/adapter/dbt_show/test_dbt_show.py
+++
@@ -1,9 +0,0 @@
-from dbt.tests.adapter.dbt_show.test_dbt_show import BaseShowSqlHeader, BaseShowLimit
-
-
-class TestBigQueryShowLimit(BaseShowLimit):
-    pass
-
-
-class TestBigQueryShowSqlHeader(BaseShowSqlHeader):
-    pass
